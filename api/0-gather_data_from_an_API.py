8c9033dc1f58ac92049d42c00e83b038410f59db
git-alexis <alex59.billemont@gmail.com>
2024-04-16 16:47:27 +0200

    Add script to gather data from an API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+from sys import argv, exit
+from requests import get
+
+if __name__ == "__main__":
+
+    if len(argv) < 2:
+        print("Usage: {} employee_id".format(argv[0]))
+        exit(1)
+
+    "Take url"
+    user_id = argv[1]
+    url_1 = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    user = get(url_1).json()
+    url_2 = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        user_id)
+    todos = get(url_2).json()
+
+    completed = []
+    for task in todos:
+        if task.get("completed") is True:
+            completed.append(task)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+
+    for task in completed:
+        print("\t {}".format(task.get("title")))
