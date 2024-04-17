08d495ff8931ca58814e5c9fc252d119a58eed90
git-alexis <alex59.billemont@gmail.com>
2024-04-17 13:26:26 +0200

    fix pycodestyle

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
         """ Creates a CSV object to format the CSV file """
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for task in todos:
-            """ Writes line with the imformation of task to the CSV file 
+            """ Writes line with the imformation of task to the CSV file
                   """
             writer.writerow([user_id, user.get("username"),
                              task.get("completed"), task.get("title")])
