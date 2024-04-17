bbc79bb5c9e158f9c2f7d1529e2da28c99229e6a
git-alexis <alex59.billemont@gmail.com>
2024-04-17 11:29:40 +0200

    re-change import module

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 
-import requests
-import sys
+from sys import argv, exit
+from requests import get
 
 if __name__ == "__main__":
 
-    if len(sys.argv) < 2:
+    if len(argv) < 2:
         print("Usage: {} employee_id".format(argv[0]))
         exit(1)
 
-    user_id = sys.argv[1]
+    user_id = argv[1]
     url_1 = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    user = requests.get(url_1).json()
+    user = get(url_1).json()
     url_2 = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
         user_id)
-    todos = requests.get(url_2).json()
+    todos = get(url_2).json()
 
     completed = []
     for task in todos:
